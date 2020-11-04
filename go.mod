7a691a1a036b5ef8cf27883a23daf6418d524f18
Philipp Mieden <dreadl0ck@protonmail.ch>
2020-11-04 11:20:37 +0100

    hello world

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/dreadl0ck/os3labgen
+
+go 1.15
+
+require (
+	code.sajari.com/docconv v1.1.0
+	github.com/JalfResi/justext v0.0.0-20170829062021-c0282dea7198 // indirect
+	github.com/advancedlogic/GoOse v0.0.0-20200830213114-1225d531e0ad // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/levigross/exp-html v0.0.0-20120902181939-8df60c69a8f5 // indirect
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
+)
---
+++ main.go
@@ -0,0 +1,137 @@
+/*
+ * OS3LABGEN - Dead simple pdf to dokuwiki lab template generator for OS3 students
+ * Copyright (c) 2020 Philipp Mieden <dreadl0ck [at] protonmail [dot] ch>
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"code.sajari.com/docconv"
+)
+
+const version = "v1.0"
+
+func main() {
+
+	// check args
+	if len(os.Args) < 2 {
+		fmt.Println("Dead simple tool to convert a PDF document with OS3 lab exercises into a dokuwiki template.")
+		fmt.Println("Provide the pdf as first argument, the resulting dokuwiki markup will be written to stdout.")
+		log.Fatal("usage: os3labgen <lab.pdf>")
+	}
+
+	// extract text from pdf
+    res, err := docconv.ConvertPath(os.Args[1])
+    if err != nil {
+        log.Fatal(err)
+	}
+
+	// print header
+	fmt.Println("==== Lab Template for", os.Args[1], "====")
+	fmt.Println()
+	fmt.Println("> generated at", time.Now().UTC())
+	fmt.Println("> with https://github.com/dreadl0ck/os3labgen", version)
+	fmt.Println()
+	
+	// setup a few variables for state keeping
+	var (
+		taskStarted = false
+		nlCount = 0
+		foundAbstract = false
+		previousTaskComplete = false
+	)
+
+	// for debugging
+	//fmt.Println(ansi.Red, res.Body, ansi.Reset)
+
+	// iterate over pdf text line by line
+    for _, line := range strings.Split(res.Body, "\n") {
+		
+		// stop when hitting refs
+		if strings.HasPrefix(line, "References") {
+			// done
+			break
+		}
+
+		// include abstract text after the header
+		if strings.HasPrefix(line, "Abstract") {
+			foundAbstract = true
+			continue
+		}
+		if foundAbstract {
+			if strings.HasPrefix(line, "Task") {
+				foundAbstract = false
+				fmt.Print("\n=== ", line)
+				taskStarted = true
+				continue
+			}
+			fmt.Println(line)
+			continue
+		}
+
+		// for debugging
+		//fmt.Println(ansi.Red, line, ansi.Reset)
+		//fmt.Println(ansi.Yellow, taskStarted, nlCount, ansi.Reset)
+
+		// process tasks
+		if line == "" {
+
+			// count newlines
+			nlCount++
+			
+			// two consequtive newlines end a task description
+			if nlCount == 1 {
+				taskStarted = false
+				nlCount = 0
+				continue
+			}
+		}
+
+		// task has begun
+		if taskStarted {
+			// check if line belongs to a task or starts a new one
+			if !strings.HasPrefix(line, "Task") {
+				if len(line) > 0 {
+					fmt.Print(" ", line)
+					previousTaskComplete = false
+				}
+			} else {
+				taskStarted = false
+				nlCount = 0
+				fmt.Println(" ===", "\n")
+				previousTaskComplete = true
+			}
+		}
+
+		// check if line is the start of a task
+		if strings.HasPrefix(line, "Task") && len(line) > 0 {
+
+			// check if the previous task line was closed properly
+			if !previousTaskComplete {
+				fmt.Println(" ===", "\n")
+				previousTaskComplete = true
+			}
+
+			// start new task
+			fmt.Print("\n=== ", line)
+			taskStarted = true
+		}		
+	}
+
+	// close the last task
+	fmt.Println(" ===")
+}
